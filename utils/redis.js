16f792494ffc48d5b95466e858bd9431fe52c531
oumaima <oumaimanaanaapro@gmail.com>
2024-08-03 14:54:20 +0100

    0

---
+++ utils/redis.js
@@ -0,0 +1,39 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.error(err);
+    });
+  //   this.getAsync = promisify(this.client.get).bind(this.client);
+  //   this.setAsync = promisify(this.client.set).bind(this.client);
+  //   this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+const redisClient = new RedisClient();
+module.exports = redisClient;

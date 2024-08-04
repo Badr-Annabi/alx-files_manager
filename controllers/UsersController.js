aec536f5f2f78c8aeb167c798de0242d2af3af1a
oumaima <oumaimanaanaapro@gmail.com>
2024-08-04 23:41:23 +0100

    3 1

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,27 +4,31 @@ import dbClient from '../utils/db';
 async function postNew(req, res) {
   // await dbClient.connect();
 
-  const { email, password } = req.body;
+  const { email } = req.body;
+  const { password } = req.body;
   // console.log(email);
   // console.log(password);
   if (!email) {
-    res.status(400).send({ error: 'Missing email' });
+    res.status(400).json({ error: 'Missing email' });
   }
   if (!password) {
-    res.status(400).send({ error: 'Missing password' });
+    res.status(400).json({ error: 'Missing password' });
   }
-  const existedEmail = await dbClient.usersCollection.findOne({ email });
-  if (existedEmail) {
-    res.status(400).send({ error: 'Already exist' });
-  }
-  const hashedPassword = sha1(password);
-  const result = await dbClient.usersCollection.insertOne({
-    email,
-    password: hashedPassword,
-  });
-  res.status(201).json({
-    id: result.insertedId,
-    email,
+  const users = dbClient.usersCollection;
+  users.findOne({ email }, (err, user) => {
+    if (user) {
+      res.status(400).json({ error: 'Already exist' });
+    } else {
+      const hashedPassword = sha1(password);
+      users.insertOne(
+        {
+          email,
+          password: hashedPassword,
+        },
+      ).then((result) => {
+        res.status(201).json({ id: result.insertedId, email });
+      }).catch((error) => console.log(error));
+    }
   });
 }
 

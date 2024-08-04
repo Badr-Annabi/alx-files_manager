38afb0f7be4df06dcf09a8541c9ef982a9de46ff
oumaima <oumaimanaanaapro@gmail.com>
2024-08-04 23:03:54 +0100

    3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,29 +1,31 @@
-import dbClient from '../utils/db';
 import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
-export async function postNew(req, res) {
-    await dbClient.connect();
+async function postNew(req, res) {
+  // await dbClient.connect();
 
-    const { email, password } = req.body;
-    // console.log(email);
-    // console.log(password);
-    if (!email) {
-        res.status(400).send({'error': 'Missing email'});
-    }
-    if (!password) {
-        res.status(400).send({'error': 'Missing password'});
-    }
-    const existedEmail = await dbClient.usersCollection.findOne({ email });
-    if (existedEmail) {
-        res.status(400).send({'error': 'Already exist'});
-    }
-    const hashed_password = sha1(password);
-    const result = await dbClient.usersCollection.insertOne({
-        email,
-        password: hashed_password
-    });
-    res.status(201).json({
-        id: result.insertedId,
-        email,
-    });
+  const { email, password } = req.body;
+  // console.log(email);
+  // console.log(password);
+  if (!email) {
+    res.status(400).send({ error: 'Missing email' });
+  }
+  if (!password) {
+    res.status(400).send({ error: 'Missing password' });
+  }
+  const existedEmail = await dbClient.usersCollection.findOne({ email });
+  if (existedEmail) {
+    res.status(400).send({ error: 'Already exist' });
+  }
+  const hashedPassword = sha1(password);
+  const result = await dbClient.usersCollection.insertOne({
+    email,
+    password: hashedPassword,
+  });
+  res.status(201).json({
+    id: result.insertedId,
+    email,
+  });
 }
+
+module.exports = postNew;
--- utils/db.js
+++ utils/db.js
@@ -6,23 +6,23 @@ class DBClient {
     this.port = process.env.DB_PORT || 27017;
     this.database = process.env.DB_DATABASE || 'files_manager';
     this.client = new MongoClient(`mongodb://${this.host}:${this.port}`, { useUnifiedTopology: true, useNewUrlParser: true });
-    // this.client.connect().then(() => {
-    //   this.db = this.client.db(this.database);
-    //   this.usersCollection = this.db.collection('users');
-    //   this.filesCollection = this.db.collection('files');
-    // }).catch((err) => {
-    //   console.error(err);
-    // });
-  }
-
-  async connect() {
-    if (!this.db) {
-      await this.client.connect();
+    this.client.connect().then(() => {
       this.db = this.client.db(this.database);
       this.usersCollection = this.db.collection('users');
       this.filesCollection = this.db.collection('files');
-    }
+    }).catch((err) => {
+      console.error(err);
+    });
   }
+
+  // async connect() {
+  //   if (!this.db) {
+  //     await this.client.connect();
+  //     this.db = this.client.db(this.database);
+  //     this.usersCollection = this.db.collection('users');
+  //     this.filesCollection = this.db.collection('files');
+  //   }
+  // }
 
   isAlive() {
     return this.client && this.client.topology && this.client.topology.isConnected();

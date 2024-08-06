62df6cbf3ad22a282ef3ae483ee0b4e9495bc110
oumaima <oumaimanaanaapro@gmail.com>
2024-08-06 22:07:25 +0100

    5

--- utils/auth.js
+++ utils/auth.js
@@ -2,24 +2,19 @@ import { ObjectId } from 'mongodb';
 import redisClient from './redis';
 import dbClient from './db';
 
-
 export const getUserByToken = async (req) => {
-  const token = req.headers['x-token'];
-    if (!token) {
-      res.status(401).json({ error: 'Unauthorized' });
-    } else {
-      const authKey = `auth_${token}`;
-      const userId = await redisClient.get(authKey);
-      if (userId) {
-        dbClient.usersCollection.findOne({ _id: new ObjectId(userId) }, (err, user) => {
-          if (user) {
-            return user;
-          } else {
-            return null;
-          }
-        });
-      } else {
-        return null;
-      }
+  const token = req.header('X-Token');
+  if (!token) {
+    return null;
+  }
+  const authKey = `auth_${token}`;
+  const userId = await redisClient.get(authKey);
+  if (userId) {
+    const user = dbClient.usersCollection.findOne({ _id: new ObjectId(userId) });
+    if (user) {
+      return user;
     }
-}
+    return null;
+  }
+  return null;
+};
